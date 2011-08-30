6e6d7dda67dd91ce971c342708b6308188d0715c
Kevin Olbrich <kolbrich@icontact.com>
2011-08-30 13:48:09 -0400

    add support for a forking remote server

---
+++ lib/Cuke4PhpForkingServer.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This class allows multiple concurrent connections to the same Cuke4php port.  Each connection forks a process that 
+ * loads the same feature files and then runs the step definitions.  This is useful for cases where the Cuke4php server
+ * is deployed to a remote machine and multiple automation scripts are simultaneously connecting to the server.
+ *
+ * Uses the PEAR Net_Server package
+ **/
+
+require_once "Cuke4Php.php";
+require_once "Net/Server.php";
+require_once "Net/Server/Handler.php";
+
+class Cuke4PhpForkingServer extends Net_Server_Handler
+{
+	private $cuke4php;
+
+	function __construct($_sFeaturePath, $_iPort) {
+		$this->cuke4php = new Cuke4Php($_sFeaturePath, $_iPort);
+	}
+
+	function onReceiveData($iClientId = 0, $data = "") {
+		$output = json_encode($this->cuke4php->process($data)) . "\n";
+		$this->_server->sendData($iClientId, $output);
+	}
+}
+
+
+?>
---
+++ php_bin/cuke4php_forking_server.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+/**
+ * main entry point for starting a Cuke4Php wire server
+ * @package Cuke4Php
+ */
+
+/**
+ * load the Cuke4PhpForkingServer server
+ */
+require_once dirname(__FILE__) . "/../lib/Cuke4PhpForkingServer.php";
+
+$aOptions = getopt("p:");
+if (array_key_exists('p',$aOptions)) {
+    $iPort = $aOptions['p'];
+} else {
+    $iPort = 16816;
+}
+
+$oServer = &Net_Server::create('fork','localhost', $iPort);
+$oServerHandler = &new Cuke4PhpForkingServer(realpath($argv[$argc-1]), $iPort);
+$oServer->setCallbackObject($oServerHandler);
+$oServer->start();
+?>
